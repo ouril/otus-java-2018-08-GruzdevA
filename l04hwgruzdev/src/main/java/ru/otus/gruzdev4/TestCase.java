6935d6a4f4eb8d3304aa67b79cca9e417a2fd43d
ouril <ourilkreg@gmail.com>
2018-11-11 23:06:00 +0300

    start to build reflection

--- l04hwgruzdev/src/main/java/ru/otus/gruzdev4/TestCase.java
+++ l04hwgruzdev/src/main/java/ru/otus/gruzdev4/TestCase.java
@@ -1,5 +1,7 @@
 package ru.otus.gruzdev4;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 
 public class TestCase {
@@ -11,7 +13,18 @@ public class TestCase {
 
     void runTests () {
         Class<? extends  Object> cls = this.getClass();
-        Object[] methods = cls.getMethods();
+        Method[] methods = cls.getDeclaredMethods();
+
+        Arrays.stream(methods).forEach((method) -> {
+            String name = method.getName();
+            method.setAccessible(true);
+            Annotation[] anatatins = method.getDeclaredAnnotations();
+            Arrays.stream(anatatins).forEach(anatation -> {
+                System.out.println(anatation instanceof Test);
+                Test test = (Test) anatation;
+                System.out.println(test.skip());
+            });
+        });
         
         System.out.println(Arrays.toString(methods));
 
