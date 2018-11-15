0675712453c7634a9e9c0e3608c722e764184e7b
ouril <ourilkreg@gmail.com>
2018-11-15 23:47:07 +0300

    Added reflection with methods

--- l04hwgruzdev/src/main/java/ru/otus/gruzdev4/TestCase.java
+++ l04hwgruzdev/src/main/java/ru/otus/gruzdev4/TestCase.java
@@ -1,32 +1,55 @@
 package ru.otus.gruzdev4;
 
 import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TestCase {
+    ArrayList<Method> testMethods = new ArrayList<>();
+    ArrayList<Method> beforMethods = new ArrayList<>();
+    ArrayList<Method> afterMethods = new ArrayList<>();
 
     @Test
     public void test(){
         System.out.println("Hello");
     }
 
-    void runTests () {
+    void getTests () {
         Class<? extends  Object> cls = this.getClass();
         Method[] methods = cls.getDeclaredMethods();
 
+
         Arrays.stream(methods).forEach((method) -> {
-            String name = method.getName();
+            String methodName = method.getName();
+            System.out.println("Name of method  ->  " + methodName);
             method.setAccessible(true);
             Annotation[] anatatins = method.getDeclaredAnnotations();
             Arrays.stream(anatatins).forEach(anatation -> {
-                System.out.println(anatation instanceof Test);
-                Test test = (Test) anatation;
-                System.out.println(test.skip());
+                if(anatation instanceof Test && !((Test) anatation).skip()){
+                   testMethods.add(method);
+                }
             });
         });
-        
-        System.out.println(Arrays.toString(methods));
+    }
 
+    void runTests(){
+        getTests();
+        runTypeofMethods(testMethods);
+    }
+
+    private void runTypeofMethods(ArrayList<Method> methods) {
+        if (!methods.isEmpty()) {
+            methods.forEach((method) -> {
+                try {
+                    method.invoke(this);
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (InvocationTargetException e) {
+                    e.printStackTrace();
+                }
+            });
+        }
     }
 }
