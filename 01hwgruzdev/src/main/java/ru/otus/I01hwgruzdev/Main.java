7faf22752f0c4ae399813d0241e01406e43f8bd0
ouril <ourilkreg@gmail.com>
2018-12-13 22:18:35 +0300

    added 01 dz again

---
+++ 01hwgruzdev/src/main/java/ru/otus/I01hwgruzdev/Main.java
@@ -0,0 +1,15 @@
+package ru.otus.I01hwgruzdev;
+
+import com.google.common.collect.Lists;
+
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args){
+        List<String> langList = Lists.newArrayList("python", "java", "js");
+        for (String lang: langList) {
+            System.out.println(lang);
+        }
+    }
+}
